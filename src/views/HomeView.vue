66c96da0e10307284b98ab50f61c51f485dd7512
GuisoPiu <gruposiice.hr@gmail.com>
2022-06-27 08:09:21 -0500

    refs are reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="home">
-    My name is {{ name }} and my age is {{ age }}
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>  
     <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
   components: {},
   setup() {
-    let name = 'mario'
-    let age = 30
+    // const p = ref(null)
+    
+    const name = ref('mario')
+    const age = ref(30)
 
     const handleClick = () => {
-      console.log('you clicked me')
+      name.value = 'luigi'
+      age.value = 35
     }
 
     return { name, age, handleClick }
