de30d6c06bfb6f596ec7cb47f5bd33bb6de73724
GuisoPiu <gruposiice.hr@gmail.com>
2022-06-30 14:15:13 -0500

    computed properties

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,28 @@
 <template>
   <div class="home">
     <h1>Home</h1> 
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">click me</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">click me</button>
+    <input type="text" v-model="search">
+    <p>Search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div> 
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { ref, reactive } from 'vue'
+import { ref, computed } from 'vue'
 
 export default {
   name: 'HomeView',
   components: {},
   setup() {
-    // const p = ref(null)
-    
-    const ninjaOne = ref({name: 'mario', age: 30 })
-    // reactive cannot use primitives
-    const ninjaTwo = reactive({name: 'luigi', age: 40 })
+    const search = ref('')
+    const names = ref(['hector', 'carlos', 'macario', 'roberot', 'irma','julio'])
 
-    const updateNinjaOne = () => { 
-      ninjaOne.value.age = 35
-    }
-
-    const updateNinjaTwo = () => { 
-      ninjaTwo.name = 'Rios',
-      ninjaTwo.age = 55
-    }
-
-    return { name, ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo }
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+    return { names, search, matchingNames }
   }
 }
 </script>
