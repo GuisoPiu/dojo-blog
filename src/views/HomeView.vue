624a483f9e2b17a8766af879364a0023396f5bfa
GuisoPiu <gruposiice.hr@gmail.com>
2022-06-30 14:34:18 -0500

    watch and watchEffect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,13 @@
     <input type="text" v-model="search">
     <p>Search term - {{ search }}</p>
     <div v-for="name in matchingNames" :key="name">{{ name }}</div> 
-
+    <button @click="handleClick">stop watch</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { ref, computed } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue'
 
 export default {
   name: 'HomeView',
@@ -19,10 +19,22 @@ export default {
     const search = ref('')
     const names = ref(['hector', 'carlos', 'macario', 'roberot', 'irma','julio'])
 
+    const stopWatch = watch(search, () => {
+      console.log('input changes')
+    })
+    const stopEffect = watchEffect(() => {
+      console.log('wathcEffect function ran', search.value)
+    })
+
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+
     const matchingNames = computed(() => {
       return names.value.filter((name) => name.includes(search.value))
     })
-    return { names, search, matchingNames }
+    return { names, search, matchingNames, handleClick }
   }
 }
 </script>
